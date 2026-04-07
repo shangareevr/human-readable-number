85e100e2a7763861da5e652a67ff9ccd6772fe97
Ruslan Shangareev <sh.rusell@gmail.com>
2026-04-07 18:02:19 +0500

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  if (number === 0) return 'zero';
+
+  const ones = [
+    '',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  const teens = [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  const hundreds = Math.floor(number / 100);
+  const remainder = number % 100;
+  let result = '';
+
+  // Сотни
+  if (hundreds > 0) {
+    result += `${ones[hundreds]} hundred`;
+    if (remainder > 0) {
+      result += ' ';
+    }
+  }
+
+  // Десятки и единицы
+  if (remainder > 0) {
+    if (remainder < 10) {
+      result += ones[remainder];
+    } else if (remainder < 20) {
+      result += teens[remainder - 10];
+    } else {
+      const ten = Math.floor(remainder / 10);
+      const one = remainder % 10;
+      result += tens[ten];
+      if (one > 0) {
+        result += ` ${ones[one]}`;
+      }
+    }
+  }
+
+  return result;
 };
